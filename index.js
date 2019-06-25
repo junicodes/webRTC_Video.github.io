9562d3d6e3817d07f09a9cb7235b093a6d275a00
Mr-Juni <junipreach2017@gmail.com>
2019-06-25 16:27:11 +0100

    make it better

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 //Activate the video and audio of the browser
-// const constraints = { audio: true, video: true }
-//
-// navigator.mediaDevices.getUserMedia(constraints)
-// .then(function(stream) {
-//   /* use the stream */
-//   console.log(stream)
+const constraints = { audio: false, video: true }
+
+navigator.mediaDevices.getUserMedia(constraints)
+.then(function(stream) {
+  /* use the stream */
+  console.log(stream)
 
   //here we required the Peer Instantiate the class
   let Peer = require('simple-peer');
@@ -12,8 +12,8 @@
   let peer = new Peer({
     //Here we need to know who is initiating this connection
     initiator: location.hash === '#init',
-    trickle: false
-    // stream: stream
+    trickle: false,
+    stream: stream
   })
   peer.on('error', function (err) { console.log('error', err) })
   //here we are going to triger the peer connection and insert the generated token into the initiator token field
@@ -24,7 +24,6 @@
   //This control the receivers when accepting the connection
   document.querySelector('#connect').addEventListener('click', function (e) {
     e.preventDefault()
-
     const receiverId = JSON.parse(document.querySelector('#receiverId').value)
     console.log(receiverId);
     //this signals a conn to generate a code
@@ -45,17 +44,25 @@
     console.log(data);
     document.querySelector('#messages').textContent += `${data}\n`;
   });
-  //
+  peer.on('stream', function (stream) {
+    console.log(stream);
+   // got remote video stream, now let's show it in a video tag
+   var video = document.querySelector('video')
+   video.srcObject=stream;
+   // video.src = window.URL.createObjectURL(stream)
+   video.play()
+ })
   // peer.on('stream', function (stream) {
+  //   console.log(stream);
   //   let video = document.createElement('video')
   //   document.body.appendChild(video)
   //
   //   video.src = window.URL.createObjectURL(stream);
-  //   video.play;
+  //   video.play();
   // })
 
-// })
-// .catch(function(err) {
-//   /* handle the error */
-//   console.log(error)
-// });
+})
+.catch(function(err) {
+  /* handle the error */
+  console.log(err)
+});
