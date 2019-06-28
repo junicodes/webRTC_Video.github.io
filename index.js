4178383bb9f25ebdff9373573765e7cc210585b2
Mr-Juni <junipreach2017@gmail.com>
2019-06-28 22:22:10 +0100

    done faster

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // the user ID Token
 let initHolder;
 let receiverHolder;
-const socket = io('http://localhost:4000/');
+const socket = io('https://webrtcchat-2019.herokuapp.com/');
 
 //Control the view if the user is not on init condition
 
